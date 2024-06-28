e33f24e9df94ee08af0a3ef528f2b2f0c187911f
Mukul2244 <mukulsharma1594@gmail.com>
2024-06-28 14:43:53 +0530

    add all register controller fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,17 +27,22 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "all fields are required")
     }
 
-    const existedUser= User.findOne({
-        $or:[{username}, {email}]
+    const existedUser= await User.findOne({
+        $or:[{username}, {email}],
     })
 
     if(existedUser) {
         throw new ApiError(409,"User with email or username already exists")
     }
+    // console.log(req.files);
 
     const avatarLocalPath= req.files?.avatar[0]?.path
-    const coverImageLocalPath= req.files?.coverImage[0]?.path
+    // const coverImageLocalPath= req.files?.coverImage[0]?.path
 
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path
+    }
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
     }
@@ -61,7 +66,7 @@ const registerUser = asyncHandler(async (req, res) => {
    const createdUser=await User.findById(user._id).select(
             "-password -refreshToken"
    )
-   if(!createUser){
+   if(!createdUser){
     throw new ApiError(500,"Something went wrong while registring the user")
    }
 
