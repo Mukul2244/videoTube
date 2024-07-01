8e8248a4fb7c94104f0f93602b5b544e30827c11
Mukul2244 <mukulsharma1594@gmail.com>
2024-07-01 16:12:39 +0530

    add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,14 @@
+import {Schema,model} from "mongoose"
+
+const subscriptionSchema= new Schema({
+    subscriber:{
+        type:Schema.Types.ObjectId, // one who is subscribing
+        ref:"User"
+    },
+    channel:{
+        type:Schema.Types.ObjectId, // user's id who owns the channel
+        ref:"User"
+    }
+},{timestamps:true})
+
+const Subscription=model(Subscription,"subscriptionSchema")
