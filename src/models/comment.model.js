62db87a35a90bd0a777d0987cc1dfbb9478a483c
Mukul2244 <mukulsharma1594@gmail.com>
2024-07-03 11:40:18 +0530

    add some bug fixes

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,5 +1,5 @@
 import { Schema, model } from "mongoose";
-import mongooseAggregatePaginate from " mongoose-aggregate-paginate-v2"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
 
 const commentSchema = new Schema({
     content: {
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -11,4 +11,4 @@ const subscriptionSchema= new Schema({
     }
 },{timestamps:true})
 
-const Subscription=model("Subscription",subscriptionSchema)
+export const Subscription=model("Subscription",subscriptionSchema)
